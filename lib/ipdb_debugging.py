ab8ef665064e5a092f92430446137103500ece5c
Melvin jerry <+>
2024-09-27 14:59:26 +0300

    d

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
-
 import ipdb
 
 def plus_two(num):
-    num + 2
-    return num
+    num += 2  # Update num to be equal to num + 2
+    ipdb.set_trace()  # This will allow you to inspect the value of num in the debugger
+    return num  # Return the updated value of num
